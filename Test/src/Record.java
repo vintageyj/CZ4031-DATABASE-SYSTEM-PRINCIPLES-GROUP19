1b4b19a1746f34a9525f3e14535a409b85d5b34c
vintageyj <vintageyj@outlook.com>
2022-10-02 12:15:52 +0800

    Updated Record.java

--- Test/src/Record.java
+++ Test/src/Record.java
@@ -75,6 +75,6 @@ public class Record {
 
     @Override
     public String toString() {
-        return String.format("(%s, %.1f, %d)", String.valueOf(tconst).trim(), averageRating, numVotes);
+        return String.valueOf(tconst).trim();
     }
 }
